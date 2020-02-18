039b4902d143d2aad72c3c9950f8b31f3c69f758
chrystiancr <chrystian.cr@live.com>
2020-02-17 22:52:39 -0300

    Updating tests

--- src/main/java/br/com/chrystian/tests/FunctionalTest.java
+++ src/main/java/br/com/chrystian/tests/FunctionalTest.java
@@ -3,15 +3,14 @@ package br.com.chrystian.tests;
 import br.com.chrystian.builder.User;
 import br.com.chrystian.datadriven.UserDataProvider;
 import br.com.chrystian.service.BaseService;
-import br.com.chrystian.utils.DefaultValues;
 import io.restassured.response.Response;
-import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static br.com.chrystian.utils.DefaultValues.UNAUTHORIZED_USER;
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class FunctionalTest extends TestBase {
 
@@ -22,8 +21,8 @@ public class FunctionalTest extends TestBase {
 
         Response createUser = baseService.createUser(user.getEmail(), user.getPassword());
 
-        assertThat(createUser.statusCode(), is(HttpStatus.SC_BAD_REQUEST));
-        Assert.assertEquals(createUser.jsonPath().get("error"), DefaultValues.UNAUTHORIZED_USER);
+        assertThat(createUser.statusCode(), is(SC_BAD_REQUEST));
+        Assert.assertEquals(createUser.jsonPath().get("error"), UNAUTHORIZED_USER);
 
     }
 }
